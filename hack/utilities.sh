6cbf4b8f69bb41a8a09f01055d029c4d6b4b665c
Paul Morie <pmorie@redhat.com>
2016-12-02 01:10:47 -0500

    Add hack/verify-all.sh and make verify

--- hack/utilities.sh
+++ hack/utilities.sh
@@ -138,3 +138,12 @@ function output_does_not_contain_substring() {
   output_contains_substring "$@" && return 1
   return 0
 }
+
+# Some useful colors.
+if [[ -z "${color_start-}" ]]; then
+  declare -r color_start="\033["
+  declare -r color_red="${color_start}0;31m"
+  declare -r color_yellow="${color_start}0;33m"
+  declare -r color_green="${color_start}0;32m"
+  declare -r color_norm="${color_start}0m"
+fi
