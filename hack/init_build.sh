0a63f35487b96b646ef70aafb0db0d83e0ec6b53
Martin Maly <mmaly@google.com>
2016-12-07 12:09:53 -0800

    Ask installed dependencies for versions
    Rather than relying on a handshake with the Jenkins
    image builder to inject the versions into version files.

--- hack/init_build.sh
+++ hack/init_build.sh
@@ -18,19 +18,22 @@ set -u
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 . "${ROOT}/hack/utilities.sh" || { echo 'Cannot load bash utilities.'; exit 1; }
 
-VERSIONS="/tmp/.versions"
 GO_VERSION='1.7.3'
 HELM_VERSION='v2.0.0'
 GLIDE_VERSION='v0.12.3'
 
 function update-golang() {
   # Check version of golang
-  local current_go_version="$(cat "${VERSIONS}/go" || echo "unknown")"
+  local current="$(go version 2>/dev/null || echo "unknown")"
 
-  if [[ "${current_go_version}" == "${GO_VERSION}" ]]; then
-    echo "Golang is up-to-date: ${current_go_version}"
+  # go version prints its output in the format:
+  #   go version go1.7.3 <os>/<arch>
+  # To isolate the version, we include the leading 'go' and trailing
+  # space in the comparison.
+  if [[ "${current}" == *"go${GO_VERSION} "* ]]; then
+    echo "Golang is up-to-date: ${current}"
   else
-    echo "Upgrading golang ${current_go_version} to ${GO_VERSION}"
+    echo "Upgrading golang ${current} to ${GO_VERSION}"
 
     # Install new golang.
     local golang_url='https://storage.googleapis.com/golang'
@@ -39,18 +42,21 @@ function update-golang() {
          | tar -C /usr/local -xz \
       || { echo "Cannot upgrade golang to ${GO_VERSION}"; return 1; }
   fi
-  return 0
 }
 
 
 function update-helm() {
   # Check version of Helm
-  local current_helm_version="$(cat "${VERSIONS}/helm" || echo "unknown")"
+  local current="$(helm version --client 2>/dev/null || echo "unknown")"
 
-  if [[ "${current_helm_version}" == "${HELM_VERSION}" ]]; then
-    echo "Helm is up-to-date: ${current_helm_version}"
+  # helm version prints its output in the format:
+  #   Client: &version.Version{SemVer:"v2.0.0", GitCommit:"...", ... }
+  # To isolate the version string, we include the surrounding quotes
+  # in the comparison.
+  if [[ "${current}" == *"\"${HELM_VERSION}\""* ]]; then
+    echo "Helm is up-to-date: ${current}"
   else
-    echo "Upgrading Helm ${current_helm_version} to ${HELM_VERSION}"
+    echo "Upgrading Helm ${current} to ${HELM_VERSION}"
 
     # Install new Helm.
     local helm_url='https://storage.googleapis.com/kubernetes-helm'
@@ -63,12 +69,16 @@ function update-helm() {
 
 function update-glide() {
   # Check version of glide
-  local current_glide_version="$(cat "${VERSIONS}/glide" || echo "unknown")"
+  local current="$(glide --version 2>/dev/null || echo "unknown")"
 
-  if [[ "${current_glide_version}" == "${GLIDE_VERSION}" ]]; then
-    echo "Glide is up-to-date: ${current_glide_version}"
+  # glide version prints its output in the format:
+  #   glide version v0.12.3
+  # To isolate the version string, we include the leading space
+  # in the comparison, and ommit the trailing wildcard.
+  if [[ "${current}" == *" ${GLIDE_VERSION}" ]]; then
+    echo "Glide is up-to-date: ${current}"
   else
-    echo "Upgrading Glide ${current_glide_version} to ${GLIDE_VERSION}"
+    echo "Upgrading Glide ${current} to ${GLIDE_VERSION}"
 
     # Install new Glide.
     local glide_url='https://github.com/Masterminds/glide/releases/download/'
