0a8eda3f7272029b100e94ed62484ba3966a6f42
Morgan Bauer <mbauer@us.ibm.com>
2016-12-08 15:32:10 -0800

    comments on what's going on

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -15,7 +15,8 @@ type Config struct {
 	GenericConfig *genericapiserver.Config
 }
 
-// internal type to take advantage of typechecking in the type system
+// internal type to take advantage of typechecking in the type
+// system. I do not understand this in depth. Rolling with it.
 type completedConfig struct {
 	*Config
 }
@@ -31,7 +32,12 @@ func (c *Config) Complete() completedConfig {
 }
 
 func (c completedConfig) New() (*ServiceCatalogAPIServer, error) {
-	genericServer, err := c.Config.GenericConfig.SkipComplete().New() // completion is done in Complete, no need for a second time
+	// we need to call new on a "completed" config, which we
+	// should already have, as this is a 'completedConfig' and the
+	// only way to get here from there is by Complete()'ing. Thus
+	// we skip the complete on the underlying config and go
+	// straight to running it's New() method.
+	genericServer, err := c.Config.GenericConfig.SkipComplete().New()
 	if err != nil {
 		return nil, err
 	}
