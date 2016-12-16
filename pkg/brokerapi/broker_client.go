7d7e2e24610d65ed407e8c431b6c51355912a0d8
Brendan Melville <bmelville@google.com>
2016-12-15 16:01:30 -0800

    Move broker client into pkg/brokerapi.
    Fixes #157.

--- pkg/brokerapi/broker_client.go
+++ pkg/brokerapi/broker_client.go
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package util
+package brokerapi
 
 import (
 	model "github.com/kubernetes-incubator/service-catalog/model/service_broker"
