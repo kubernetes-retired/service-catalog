7cc79ed27c6c0fc6a1e8528fb70f6ad91ede9077
Aaron Schlesinger <aaron@deis.com>
2017-01-10 13:11:44 -0800

    adding more godocs
    to satisfy lint

--- pkg/brokerapi/fake/errors.go
+++ pkg/brokerapi/fake/errors.go
@@ -5,8 +5,16 @@ import (
 )
 
 var (
-	ErrInstanceNotFound      = errors.New("instance not found")
+	// ErrInstanceNotFound is returned by the fake client when an instance was expected in its
+	// internal storage but it wasn't found
+	ErrInstanceNotFound = errors.New("instance not found")
+	// ErrInstanceAlreadyExists is returned by the fake client when an instance was found in
+	// internal storage but it wasn't expected
 	ErrInstanceAlreadyExists = errors.New("instance already exists")
-	ErrBindingNotFound       = errors.New("binding not found")
-	ErrBindingAlreadyExists  = errors.New("binding already exists")
+	// ErrBindingNotFound is returned by the fake client when a binding was expected in internal
+	// storage but it wasn't found
+	ErrBindingNotFound = errors.New("binding not found")
+	// ErrBindingAlreadyExists is returned by the fake client when a binding was found in internal
+	// storage but it wasn't expected
+	ErrBindingAlreadyExists = errors.New("binding already exists")
 )
