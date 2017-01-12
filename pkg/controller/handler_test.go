e840c8e949d6ed1942e603877563d05b3d505ae1
Aaron Schlesinger <aaron@deis.com>
2017-01-12 15:18:29 -0800

    fixing vet and boilerplate issues

--- pkg/controller/handler_test.go
+++ pkg/controller/handler_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controller
 
 import (
@@ -19,7 +35,7 @@ func TestCreateServiceInstanceHelper(t *testing.T) {
 	// set up the mock (in-memory) storage implementation
 	storage := mem.NewPopulatedStorage(
 		map[string]*servicecatalog.Broker{
-			brokerName: &servicecatalog.Broker{
+			brokerName: {
 				ObjectMeta: api.ObjectMeta{
 					Namespace: namespace,
 					Name:      brokerName,
@@ -27,7 +43,7 @@ func TestCreateServiceInstanceHelper(t *testing.T) {
 			},
 		},
 		map[string]*servicecatalog.ServiceClass{
-			svcClassName: &servicecatalog.ServiceClass{
+			svcClassName: {
 				ObjectMeta: api.ObjectMeta{
 					Namespace: namespace,
 					Name:      svcClassName,
