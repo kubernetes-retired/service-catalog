0d82c77d940a5f2a5a46613643d62e35f0c5946e
Aaron Schlesinger <aaron@deis.com>
2017-01-12 13:34:46 -0800

    get a basic test up and running

--- pkg/controller/handler_test.go
+++ pkg/controller/handler_test.go
@@ -7,19 +7,42 @@ import (
 	"github.com/kubernetes-incubator/service-catalog/pkg/brokerapi/fake"
 	"github.com/kubernetes-incubator/service-catalog/pkg/controller/injector"
 	"github.com/kubernetes-incubator/service-catalog/pkg/controller/storage/mem"
+	"k8s.io/kubernetes/pkg/api"
 )
 
 func TestCreateServiceInstanceHelper(t *testing.T) {
-	storage := mem.NewStorage()
+	const (
+		brokerName   = "testBroker"
+		svcClassName = "testSvcClass"
+	)
+	storage := mem.NewPopulatedStorage(
+		map[string]*servicecatalog.Broker{
+			brokerName: &servicecatalog.Broker{
+				ObjectMeta: api.ObjectMeta{
+					Name: brokerName,
+				},
+			},
+		},
+		map[string]*servicecatalog.ServiceClass{
+			svcClassName: &servicecatalog.ServiceClass{
+				ObjectMeta: api.ObjectMeta{
+					Name: svcClassName,
+				},
+				BrokerName: brokerName,
+			},
+		},
+	)
 	inj := injector.NewFake()
 	catalogCl := fake.CatalogClient{}
-	instanceCl := fake.InstanceClient{}
-	bindingCl := fake.BindingClient{}
-	brokerClFunc := fake.NewClientFunc(catalogCl, instanceCl, bindingCl)
+	instanceCl := fake.NewInstanceClient()
+	bindingCl := fake.NewBindingClient()
+	brokerClFunc := fake.NewClientFunc(catalogCl, *instanceCl, *bindingCl)
 	hdl := createHandler(storage, inj, brokerClFunc)
 
 	inst := &servicecatalog.Instance{
-		Spec:   servicecatalog.InstanceSpec{},
+		Spec: servicecatalog.InstanceSpec{
+			ServiceClassName: svcClassName,
+		},
 		Status: servicecatalog.InstanceStatus{},
 	}
 	if err := hdl.createServiceInstance(inst); err != nil {
