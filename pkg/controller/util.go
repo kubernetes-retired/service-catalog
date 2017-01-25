00e62153c12b3845b49e7da29cc9ea90260726e2
Aaron Schlesinger <aaron@deis.com>
2017-01-25 15:02:26 -0700

    remove dead code

--- pkg/controller/util.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import (
-	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog"
-)
-
-func checkBindingCondition(sb *servicecatalog.Binding, bc servicecatalog.BindingCondition, ct servicecatalog.ConditionStatus) bool {
-	return false
-}
