ae08473fc467646a1277440ee166b92f5724de1f
Paul Morie <pmorie@redhat.com>
2017-01-25 17:46:34 -0500

    Wire storage factory and Broker, ServiceClass, Binding

--- pkg/registry/servicecatalog/broker/storage.go
+++ pkg/registry/servicecatalog/broker/storage.go
@@ -61,14 +61,25 @@ func GetAttrs(obj runtime.Object) (labels.Set, fields.Set, error) {
 func NewStorage(opts generic.RESTOptions) rest.Storage {
 	prefix := "/" + opts.ResourcePrefix
 
+	newListFunc := func() runtime.Object { return &servicecatalog.BrokerList{} }
+	storageInterface, dFunc := opts.Decorator(
+		opts.StorageConfig,
+		1000,
+		&servicecatalog.Broker{},
+		prefix,
+		brokerRESTStrategies,
+		newListFunc,
+		nil,
+		storage.NoTriggerPublisher,
+	)
+
 	store := registry.Store{
 		NewFunc: func() runtime.Object {
 			return &servicecatalog.Broker{}
 		},
 		// NewListFunc returns an object capable of storing results of an etcd list.
-		NewListFunc: func() runtime.Object {
-			return &servicecatalog.BrokerList{}
-		},
+		NewListFunc: newListFunc,
+
 		// Produces a path that etcd understands, to the root of the resource
 		// by combining the namespace in the context with the given prefix
 		KeyRootFunc: func(ctx api.Context) string {
@@ -91,6 +102,9 @@ func NewStorage(opts generic.RESTOptions) rest.Storage {
 		CreateStrategy: brokerRESTStrategies,
 		UpdateStrategy: brokerRESTStrategies,
 		DeleteStrategy: brokerRESTStrategies,
+
+		Storage:     storageInterface,
+		DestroyFunc: dFunc,
 	}
 
 	return &store
