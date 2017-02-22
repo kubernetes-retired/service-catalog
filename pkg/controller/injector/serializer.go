0f7b7c37c7000df7696b902b3bb5140ed0a6580c
Paul Morie <pmorie@redhat.com>
2017-02-22 14:02:17 -0500

    Port behavior into wip-controller

--- pkg/controller/injector/serializer.go
+++ pkg/controller/injector/serializer.go
@@ -20,26 +20,27 @@ import (
 	"encoding/json"
 )
 
-// serialize converts values of any type to a []byte, suitable for addition to a
-// k8s Secret's Data field (which is a map[string][]byte). Generally, this
-// serialization is performed using json.Unmarshal(), which easily handles most
-// primitives, but will also, conveniently, handle the scenario where the item
-// being serialized is a map[string]interface{} or []interface{}-- both of which
-// are possible since an OSB broker's response to a binding request might
-// contain credentials that are composed of arbitrarily complex JSON. These
-// would have been unmarshalled to map[string]interface{} or []interface{} when
-// they were received and need to be re-marshalled here before they can be added
-// to a Secret. (Note that the consumer would need to know what to do with those
-// bytes that contain arbitrarily complex JSON. The controller's only concern is
-// accurately passing along all credentials received from the OSB broker.) The
-// common case where this strategy doesn't work is that of string values. Using
-// json.Unmarshal() to serialize string values results in a []byte where the
-// leading and trailing bytes represent double quotes. Consequently, this would
-// lead to consumers receiving a credential like `"password"` where the
-// credential from the OSB broker was actually `password`. It's easy to see
-// that's bad, so an alternative strategy is used for serializing string values
-// to avoid introducing those errant quotes.
-func serialize(value interface{}) ([]byte, error) {
+// Serialize converts values of any type to a []byte, suitable for addition to
+// a k8s Secret's Data field (which is a map[string][]byte). Generally, this
+// serialization is performed using json.Unmarshal(), which easily handles
+// most primitives, but will also, conveniently, handle the scenario where the
+// item being serialized is a map[string]interface{} or []interface{}-- both
+// of which are possible since an OSB broker's response to a binding request
+// might contain credentials that are composed of arbitrarily complex JSON.
+// These would have been unmarshalled to map[string]interface{} or
+// []interface{} when they were received and need to be re-marshalled here
+// before they can be added to a Secret. (Note that the consumer would need to
+// know what to do with those bytes that contain arbitrarily complex JSON. The
+// controller's only concern is accurately passing along all credentials
+// received from the OSB broker.) The common case where this strategy doesn't
+// work is that of string values. Using json.Unmarshal() to serialize string
+// values results in a []byte where the leading and trailing bytes represent
+// double quotes. Consequently, this would lead to consumers receiving a
+// credential like `"password"` where the credential from the OSB broker was
+// actually `password`. It's easy to see that's bad, so an alternative
+// strategy is used for serializing string values to avoid introducing those
+// errant quotes.
+func Serialize(value interface{}) ([]byte, error) {
 	if strVal, ok := value.(string); ok {
 		return []byte(strVal), nil
 	}
