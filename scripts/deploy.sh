cb4041c683f12c74b96c09b71316da1365c973b7
Aaron Schlesinger <aaron@deis.com>
2017-03-13 12:24:25 -0700

    adding set flags to the top of the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 export REGISTRY=quay.io/kubernetes-service-catalog
 
 if [[ -n "${TRAVIS_TAG}" ]]; then
