7a88cf53cd6c4cd1a28f6c70581c9bafe3763ba0
Kent Rancourt <krancour@users.noreply.github.com>
2017-04-17 17:33:15 -0400

    use keyer to construct keys for test (#2)

--- pkg/storage/tpr/storage_interface_test.go
+++ pkg/storage/tpr/storage_interface_test.go
@@ -27,6 +27,8 @@ import (
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/diff"
+	"k8s.io/apiserver/pkg/endpoints/request"
+	restclient "k8s.io/client-go/rest"
 )
 
 const (
@@ -35,7 +37,9 @@ const (
 )
 
 func TestCreate(t *testing.T) {
-	iface, fakeCl := getTPRStorageIFaceAndFakeCoreRESTClient(t)
+	keyer := getKeyer()
+	fakeCl := newFakeCoreRESTClient()
+	iface := getTPRStorageIFace(t, keyer, fakeCl)
 	broker := &v1alpha1.Broker{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: v1alpha1.SchemeGroupVersion.String(),
@@ -43,10 +47,14 @@ func TestCreate(t *testing.T) {
 		},
 		ObjectMeta: metav1.ObjectMeta{Name: name},
 	}
+	key, err := keyer.Key(request.NewContext(), name)
+	if err != nil {
+		t.Fatalf("error constructing key (%s)", err)
+	}
 	outBroker := &v1alpha1.Broker{}
 	if err := iface.Create(
 		context.Background(),
-		name,
+		key,
 		broker,
 		outBroker,
 		uint64(0),
@@ -59,7 +67,7 @@ func TestCreate(t *testing.T) {
 	}
 	// Confirm the output is identical to what is in storage (nothing funny
 	// happened during encoding / decoding the response).
-	obj := fakeCl.storage.get(name, ServiceBrokerKind.URLName(), name)
+	obj := fakeCl.storage.get(namespace, ServiceBrokerKind.URLName(), name)
 	if obj == nil {
 		t.Fatal("no broker was in storage")
 	}
@@ -98,23 +106,23 @@ func TestRemoveNamespace(t *testing.T) {
 	}
 }
 
-func getTPRStorageIFaceAndFakeCoreRESTClient(
-	t *testing.T,
-) (*store, *fakeCoreRESTClient) {
-	keyer := Keyer{
+func getKeyer() Keyer {
+	return Keyer{
 		DefaultNamespace: namespace,
 		ResourceName:     ServiceBrokerKind.String(),
 		Separator:        "/",
 	}
+}
+
+func getTPRStorageIFace(t *testing.T, keyer Keyer, restCl restclient.Interface) *store {
 	codec, err := testapi.GetCodecForObject(&v1alpha1.Broker{})
-	fakeCl := newFakeCoreRESTClient()
 	if err != nil {
 		t.Fatalf("error getting codec (%s)", err)
 	}
 	return &store{
 		decodeKey:    keyer.NamespaceAndNameFromKey,
 		codec:        codec,
-		cl:           fakeCl,
+		cl:           restCl,
 		singularKind: ServiceBrokerKind,
-	}, fakeCl
+	}
 }
