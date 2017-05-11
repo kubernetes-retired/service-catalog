48544045000fbb54b0100e9278235e6b375a055d
Aaron Schlesinger <aaschles@microsoft.com>
2017-05-11 15:12:00 -0700

    adding tests for the metadata accessors

---
+++ pkg/storage/tpr/metadata_access_test.go
@@ -0,0 +1,143 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tpr
+
+import (
+	"testing"
+	"time"
+
+	sc "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestGetAccessor(t *testing.T) {
+	if GetAccessor() != accessor {
+		t.Fatalf("GetAccessor didn't return the pre-initialized accessor")
+	}
+}
+
+func TestGetNamespace(t *testing.T) {
+	const namespace = "testns"
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: namespace,
+		},
+	}
+	ns, err := GetNamespace(obj)
+	if err != nil {
+		t.Fatalf("error getting namespace (%s)", err)
+	}
+	if ns != namespace {
+		t.Fatalf("actual namespace (%s) wasn't expected (%s)", ns, namespace)
+	}
+}
+
+func TestDeletionTimestampExists(t *testing.T) {
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{},
+	}
+	exists, err := deletionTimestampExists(obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatalf("deletion timestamp reported as exists when it didn't")
+	}
+	tme := metav1.NewTime(time.Now())
+	obj.DeletionTimestamp = &tme
+	exists, err = deletionTimestampExists(obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatal("deletion timestamp reported as missing when it isn't")
+	}
+}
+
+func TestDeletionGracePeriodExists(t *testing.T) {
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{},
+	}
+	exists, err := deletionGracePeriodExists(obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if exists {
+		t.Fatal("deletion grace period reported as exists when it didn't")
+	}
+	sec := int64(1)
+	obj.DeletionGracePeriodSeconds = &sec
+	exists, err = deletionGracePeriodExists(obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !exists {
+		t.Fatalf("deletion grace period reported as missing when it isn't")
+	}
+}
+
+func TestGetFinalizers(t *testing.T) {
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{Finalizers: []string{tprFinalizer}},
+	}
+	finalizers, err := getFinalizers(obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(finalizers) != 1 {
+		t.Fatalf("expected 1 finalizer, got %s", len(finalizers))
+	}
+	if finalizers[0] != tprFinalizer {
+		t.Fatalf("expected finalizer %s, got %s", tprFinalizer, finalizers[0])
+	}
+}
+
+func TestAddFinalizer(t *testing.T) {
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{},
+	}
+	if err := addFinalizer(obj, tprFinalizer); err != nil {
+		t.Fatal(err)
+	}
+	if len(obj.Finalizers) != 1 {
+		t.Fatalf("expected 1 finalizer, got %d", len(obj.Finalizers))
+	}
+	if obj.Finalizers[0] != tprFinalizer {
+		t.Fatalf("expected finalizer %s, got %s", tprFinalizer, obj.Finalizers[0])
+	}
+}
+
+func TestRemoveFinalizer(t *testing.T) {
+	obj := &sc.Instance{
+		ObjectMeta: metav1.ObjectMeta{Finalizers: []string{tprFinalizer}},
+	}
+	if err := removeFinalizer(obj, tprFinalizer+"-noexist"); err != nil {
+		t.Fatalf("error removing non-existent finalizer (%s)", err)
+	}
+	if len(obj.Finalizers) != 1 {
+		t.Fatalf("finalizer was removed when it shouldn't have been")
+	}
+	if obj.Finalizers[0] != tprFinalizer {
+		t.Fatalf("expected finalizer %s, got %s", tprFinalizer, obj.Finalizers[0])
+	}
+	if err := removeFinalizer(obj, tprFinalizer); err != nil {
+		t.Fatalf("error removing existent finalizer (%s)", err)
+	}
+	if len(obj.Finalizers) != 0 {
+		t.Fatalf("expected no finalizers, got %d", len(obj.Finalizers))
+	}
+}
