c1dd8f6ee298d3b49be47b388dd7ff0811fc6570
Aaron Schlesinger <aaschles@microsoft.com>
2017-05-22 12:32:42 -0700

    sending the appropriate query string parameters in deprovision
    … and not sending a request body
    
    Fixes https://github.com/kubernetes-incubator/service-catalog/issues/883

--- pkg/brokerapi/openservicebroker/open_service_broker_client.go
+++ pkg/brokerapi/openservicebroker/open_service_broker_client.go
@@ -194,11 +194,15 @@ func (c *openServiceBrokerClient) DeleteServiceInstance(ID string, req *brokerap
 	}
 
 	// TODO: Handle the auth
-	resp, err := sendOSBRequest(c, http.MethodDelete, serviceInstanceURL, req)
+	httpReq, err := c.newOSBRequest(http.MethodDelete, serviceInstanceURL, nil)
 	if err != nil {
-		glog.Errorf("Error sending delete service instance request to broker %q at %v: response: %v error: %#v", c.name, serviceInstanceURL, resp, err)
-		return nil, resp.StatusCode, errRequest{message: err.Error()}
+		glog.Errorf("Error creating delete service instance request to broker %q at %v (%#v)", c.name, serviceInstanceURL, err)
+		return nil, 0, errRequest{message: err.Error()}
 	}
+	httpReq.Header.Set("service_id", req.ServiceID)
+	httpReq.Header.Set("plan_id", req.PlanID)
+	httpReq.Header.Set("accepts_incomplete", fmt.Sprintf("%t", req.AcceptsIncomplete))
+	resp, err := c.Client.Do(httpReq)
 	defer resp.Body.Close()
 
 	deleteServiceInstanceResponse := brokerapi.DeleteServiceInstanceResponse{}
