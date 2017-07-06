f2567e30b55ce38f83e6a0e7f42bc40a844d7d90
Crismar Mejia <crismar.mejia@gmail.com>
2017-07-06 17:21:54 -0400

    initial work on the test

---
+++ pkg/rest/core/fake/rest_client_test.go
@@ -0,0 +1,88 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fake
+
+import (
+	"testing"
+
+	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog"
+)
+
+func TestNamespacedStorageSetDelete(t *testing.T) {
+	storage := make(NamespacedStorage)
+
+	if nil != storage.Get("ns", "tipe", "name") {
+		t.Fatal("Expected no results from an empty storage")
+	}
+
+	storage.Set("ns", "tipe", "name", &servicecatalog.Broker{})
+	if nil == storage.Get("ns", "tipe", "name") {
+		t.Fatal("Expected a object to be stored")
+	}
+
+	storage.Delete("ns", "tipe", "name")
+	if nil != storage.Get("ns", "tipe", "name") {
+		t.Fatal("Expected the object to be deleted")
+	}
+}
+
+func TestNamespacedStorageGetList(t *testing.T) {
+	storage := make(NamespacedStorage)
+	storage.Set("ns1", "type1", "broker1", &servicecatalog.Broker{})
+	storage.Set("ns1", "type1", "broker2", &servicecatalog.Broker{})
+	storage.Set("ns1", "type1", "broker3", &servicecatalog.Broker{})
+
+	storage.Set("ns2", "type1", "broker1", &servicecatalog.Broker{})
+	storage.Set("ns2", "type1", "broker2", &servicecatalog.Broker{})
+
+	objects := storage.GetList("ns1", "type1")
+	if count := len(objects); count != 3 {
+		t.Fatal("Expected ", 3, "got", count)
+	}
+}
+
+func TestResponseWriter(t *testing.T) {
+	rw := newResponseWriter()
+	length, err := rw.Write([]byte{0, 0, 0})
+	if err != nil {
+		t.Fatal("Error writing response", err)
+	}
+
+	if length != 3 {
+		t.Fatal("Expected length", 3, "got", length)
+	}
+}
+
+func TestResponseWriterGetResponsePanic(t *testing.T) {
+	rw := newResponseWriter()
+	// no hearder status set should panic
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("Expected a panic")
+		}
+	}()
+	rw.getResponse()
+}
+
+func TestResponseWriterGetResponse(t *testing.T) {
+	rw := newResponseWriter()
+	rw.WriteHeader(200)
+
+	if response := rw.getResponse(); response.StatusCode != 200 {
+		t.Fatal("Expected", 0, "got", response.StatusCode)
+	}
+}
