9d2139ef6d05e5edeb5383033f1e407b97077068
Crismar Mejia <crismar.mejia@gmail.com>
2017-07-10 16:58:21 -0400

    added more test, helpers

--- pkg/rest/core/fake/rest_client_test.go
+++ pkg/rest/core/fake/rest_client_test.go
@@ -17,41 +17,80 @@ limitations under the License.
 package fake
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"testing"
 
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog"
 )
 
+const (
+	ns1   = "ns1"
+	ns2   = "ns2"
+	tipe1 = "tipe1"
+	name1 = "name1"
+	name2 = "name2"
+	name3 = "name3"
+)
+
+//Helpers
+func createSingleItemStorage() NamespacedStorage {
+	storage := make(NamespacedStorage)
+	storage.Set(ns1, tipe1, name1, &servicecatalog.Broker{})
+	return storage
+}
+
+func createMultipleItemStorage() NamespacedStorage {
+	storage := make(NamespacedStorage)
+	storage.Set(ns1, tipe1, name1, &servicecatalog.Broker{})
+	storage.Set(ns1, tipe1, name2, &servicecatalog.Broker{})
+	storage.Set(ns1, tipe1, name3, &servicecatalog.Broker{})
+
+	storage.Set(ns2, tipe1, name1, &servicecatalog.Broker{})
+	storage.Set(ns2, tipe1, name2, &servicecatalog.Broker{})
+
+	return storage
+
+}
+
+func createURL(namespace, tipe, name string) string {
+	if name == "" {
+		return fmt.Sprintf("/apis/servicecatalog.k8s.io/v1alpha1/namespaces/%s/%s", namespace, tipe)
+	}
+	return fmt.Sprintf("/apis/servicecatalog.k8s.io/v1alpha1/namespaces/%s/%s/%s", namespace, tipe, name)
+}
+
 func TestNamespacedStorageSetDelete(t *testing.T) {
 	storage := make(NamespacedStorage)
 
-	if nil != storage.Get("ns", "tipe", "name") {
+	if nil != storage.Get(ns1, tipe1, name1) {
 		t.Fatal("Expected no results from an empty storage")
 	}
 
-	storage.Set("ns", "tipe", "name", &servicecatalog.Broker{})
-	if nil == storage.Get("ns", "tipe", "name") {
+	storage = createSingleItemStorage()
+
+	if storage.Get(ns1, tipe1, name1) == nil {
 		t.Fatal("Expected a object to be stored")
 	}
 
-	storage.Delete("ns", "tipe", "name")
-	if nil != storage.Get("ns", "tipe", "name") {
+	storage.Delete(ns1, tipe1, name1)
+	if nil != storage.Get(ns1, tipe1, name1) {
 		t.Fatal("Expected the object to be deleted")
 	}
 }
 
 func TestNamespacedStorageGetList(t *testing.T) {
-	storage := make(NamespacedStorage)
-	storage.Set("ns1", "type1", "broker1", &servicecatalog.Broker{})
-	storage.Set("ns1", "type1", "broker2", &servicecatalog.Broker{})
-	storage.Set("ns1", "type1", "broker3", &servicecatalog.Broker{})
+	storage := createMultipleItemStorage()
 
-	storage.Set("ns2", "type1", "broker1", &servicecatalog.Broker{})
-	storage.Set("ns2", "type1", "broker2", &servicecatalog.Broker{})
-
-	objects := storage.GetList("ns1", "type1")
+	objects := storage.GetList(ns1, tipe1)
 	if count := len(objects); count != 3 {
 		t.Fatal("Expected ", 3, "got", count)
+	}
+
+	objects = storage.GetList(ns2, tipe1)
+	if count := len(objects); count != 2 {
+		t.Fatal("Expected ", 2, "got", count)
 	}
 }
 
@@ -80,9 +119,52 @@ func TestResponseWriterGetResponsePanic(t *testing.T) {
 
 func TestResponseWriterGetResponse(t *testing.T) {
 	rw := newResponseWriter()
-	rw.WriteHeader(200)
+	rw.WriteHeader(http.StatusFound)
 
-	if response := rw.getResponse(); response.StatusCode != 200 {
+	if response := rw.getResponse(); response.StatusCode != http.StatusFound {
 		t.Fatal("Expected", 0, "got", response.StatusCode)
 	}
+}
+
+func TestGetItemEmptyStorage(t *testing.T) {
+	storage := make(NamespacedStorage)
+	rw := newResponseWriter()
+	url := createURL(ns1, tipe1, name1)
+	request, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f := getItem(storage)
+	f(rw, request)
+
+	if rw.getResponse().StatusCode != http.StatusNotFound && rw.headerSet {
+		t.Fatal("Expected NotFoundStatus for empty storage")
+	}
+
+}
+
+func TestGetItem(t *testing.T) {
+	storage := createSingleItemStorage()
+	rw := newResponseWriter()
+	url := createURL(ns1, tipe1, name1)
+	request, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f := getItem(storage)
+	f(rw, request)
+
+	if rw.getResponse().StatusCode != http.StatusOK && rw.headerSet {
+		t.Fatal("Expected Status OK", http.StatusOK, "got", rw.getResponse().StatusCode)
+	}
+	_, err = ioutil.ReadAll(rw.getResponse().Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
