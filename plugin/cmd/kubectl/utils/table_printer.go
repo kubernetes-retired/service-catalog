d6066ca3a8eec2ed5f9174e24894870a201202d3
Doug Davis <dug@us.ibm.com>
2017-07-11 11:44:32 -0700

    Fix some more "verify" issues
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- plugin/cmd/kubectl/utils/table_printer.go
+++ plugin/cmd/kubectl/utils/table_printer.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package utils
 
 import (
@@ -6,6 +22,7 @@ import (
 	"text/tabwriter"
 )
 
+// Table defines a tabular output - obviously in table format
 type Table struct {
 	headers []string
 	rows    [][]string
@@ -18,10 +35,12 @@ func NewTable(headers ...string) *Table {
 	}
 }
 
+// AddRow will append the specified row to the table
 func (t *Table) AddRow(row ...string) {
 	t.rows = append(t.rows, row)
 }
 
+// Print prints the table to the screen
 func (t *Table) Print() error {
 	padding := 3
 
