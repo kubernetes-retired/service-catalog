def66d1b03043afd377501d8c1cc042496d7cd1b
Erik Nelson <erik@nsk.io>
2017-09-07 17:09:35 -0400

    Fix bug in the defaults with Duration
    As written, was always setting Duration to 15m0s when Behavior was set
    to Duration

--- pkg/apis/servicecatalog/v1alpha1/defaults.go
+++ pkg/apis/servicecatalog/v1alpha1/defaults.go
@@ -32,7 +32,7 @@ func SetDefaults_ServiceBrokerSpec(spec *ServiceBrokerSpec) {
 		spec.RelistBehavior = ServiceBrokerRelistBehaviorDuration
 	}
 
-	if spec.RelistBehavior == ServiceBrokerRelistBehaviorDuration {
+	if spec.RelistDuration == nil {
 		spec.RelistDuration = &metav1.Duration{15 * time.Minute}
 	}
 }
