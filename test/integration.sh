31ae521203d35baaedb03c49701de97b5e91edc4
Jeff Peeler <jpeeler@redhat.com>
2017-11-14 15:03:50 -0500

    Check if file permissions allow go install (#1566)
    Currently the integration tests install package dependencies regardless
    of whether or not GOROOT permissions allow it. This change will check
    for write access on the appropriate directory and if write permissions
    aren't found, the -i flag is dropped.
    
    Since there could be a scenario where one wants to install the
    dependencies and has a non-writable GOROOT, the environment variable
    PKGDIR can be defined in that case.

--- test/integration.sh
+++ test/integration.sh
@@ -21,11 +21,20 @@ set -o pipefail
 # this script resides in the `test/` folder at the root of the project
 KUBE_ROOT=$(realpath $(dirname "${BASH_SOURCE}")/../pkg/kubernetes)
 source "${KUBE_ROOT}/hack/lib/init.sh"
+GOROOT=$(go env GOROOT)
 
 runTests() {
   kube::etcd::start
 
-  go test -race -i github.com/kubernetes-incubator/service-catalog/test/integration/... -c \
+  if [[ -w ${GOROOT}/pkg ]]; then
+    FLAGS="-i"
+  elif [[ -n ${PKGDIR:-} ]]; then
+    FLAGS="-pkgdir $PKGDIR"
+  else
+    FLAGS=""
+  fi
+
+  go test -race $FLAGS github.com/kubernetes-incubator/service-catalog/test/integration/... -c \
       && ./integration.test -test.v $@
 }
 
