2a41027ec32aa8887a2bafcf4efc774c3d78fec1
Jonathan Berkhahn <jaberkha@us.ibm.com>
2017-11-14 16:55:09 -0800

    Refactor relist-service-broker to work with changes

--- plugin/cmd/kubectl/relist-service-broker/relist-service-broker.go
+++ plugin/cmd/kubectl/relist-service-broker/relist-service-broker.go
@@ -20,41 +20,28 @@ import (
 	"fmt"
 	"os"
 
-	clientset "github.com/kubernetes-incubator/service-catalog/pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1"
 	"github.com/kubernetes-incubator/service-catalog/plugin/cmd/kubectl/utils"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"k8s.io/client-go/rest"
 )
 
 const usage = `Usage:
   kubectl plugin relist-service-broker BROKER_NAME`
 
 func main() {
-	svcURL := utils.SCUrlEnv()
-
 	if len(os.Args) != 2 {
 		utils.Exit1(usage)
 	}
 
-	restConfig := rest.Config{
-		Host:    svcURL,
-		APIPath: "/apis/servicecatalog.k8s.io/v1beta1",
-	}
-
-	svcClient, err := clientset.NewForConfig(&restConfig)
-	if err != nil {
-		utils.Exit1(fmt.Sprintf("Initializing client for service catalog (%s)", err))
-	}
+	scClient, _ := utils.NewClient()
 	brokerName := os.Args[1]
-	broker, err := svcClient.ClusterServiceBrokers().Get(brokerName, v1.GetOptions{})
+	broker, err := scClient.ServicecatalogV1beta1().ClusterServiceBrokers().Get(brokerName, v1.GetOptions{})
 	if err != nil {
 		utils.Exit1(fmt.Sprintf("Unable to find broker %s (%s)", brokerName, err))
 	}
 
 	fmt.Printf("Relisting broker %s...\n", utils.Entity(broker.Name))
 	broker.Spec.RelistRequests++
-	resp, err := svcClient.ClusterServiceBrokers().Update(broker)
+	resp, err := scClient.ServicecatalogV1beta1().ClusterServiceBrokers().Update(broker)
 	if err != nil {
 		utils.Exit1(fmt.Sprintf("Updating broker resource (%s)", err))
 	}
