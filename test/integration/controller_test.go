cdb2ac7a6ca10f2958dd16c2b42372d7937ce957
staebler <staebler@redhat.com>
2017-11-16 12:54:37 -0500

    Add controller integration tests for ServiceInstance create and update

--- test/integration/controller_test.go
+++ test/integration/controller_test.go
@@ -23,6 +23,7 @@ import (
 	"testing"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -337,6 +338,11 @@ func TestAsyncProvisionWithMultiplePolls(t *testing.T) {
 		},
 	}
 	ct.run(func(ct *controllerTest) {
+		// Polling is going to take at least 3 seconds, with a 1-second break between the first poll and the second
+		// and a 2-second break between the second poll and the third. Let's sleep here so that we don't risk
+		// timing out while waiting for the instance condition to be ready in the following wait.
+		//time.Sleep(3 * time.Second)
+
 		verifyInstanceCreated(t, ct.client, ct.instance)
 	})
 }
@@ -437,6 +443,29 @@ func changeUsernameForCatalogClient(catalogClient clientset.Interface, catalogCl
 func prependGetSecretNotFoundReaction(fakeKubeClient *fake.Clientset) {
 	fakeKubeClient.PrependReactor("get", "secrets", func(action clientgotesting.Action) (bool, runtime.Object, error) {
 		return true, nil, apierrors.NewNotFound(action.GetResource().GroupResource(), action.(clientgotesting.GetAction).GetName())
+	})
+}
+
+// prependGetSecretReaction prepends a reaction to getting secrets from the fake kube client
+// that returns a secret with the specified secret data when a request is made for the secret
+// with the specified secret name.
+func prependGetSecretReaction(fakeKubeClient *fake.Clientset, secretName string, secretData map[string][]byte) {
+	fakeKubeClient.PrependReactor("get", "secrets", func(action clientgotesting.Action) (bool, runtime.Object, error) {
+		getAction, ok := action.(clientgotesting.GetAction)
+		if !ok {
+			return true, nil, apierrors.NewInternalError(fmt.Errorf("could not convert get secrets action to a GetAction: %T", action))
+		}
+		if getAction.GetName() != secretName {
+			return false, nil, nil
+		}
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: testNamespace,
+				Name:      secretName,
+			},
+			Data: secretData,
+		}
+		return true, secret, nil
 	})
 }
 
@@ -860,4 +889,15 @@ func getLastBrokerAction(t *testing.T, osbClient *fakeosb.FakeClient, actionType
 		t.Fatalf("unexpected action type: expected %s, got %s", e, a)
 	}
 	return brokerAction
+}
+
+// convertParametersIntoRawExtension converts the specified map of parameters
+// into a RawExtension object that can be used in the Parameters field of
+// ServiceInstanceSpec or ServiceBindingSpec.
+func convertParametersIntoRawExtension(t *testing.T, parameters map[string]interface{}) *runtime.RawExtension {
+	marshalledParams, err := json.Marshal(parameters)
+	if err != nil {
+		t.Fatalf("Failed to marshal parameters %v : %v", parameters, err)
+	}
+	return &runtime.RawExtension{Raw: marshalledParams}
 }
