4c430d87de3762759d87a4b76aca734bbfe760ff
Nail Islamov <nislamov@atlassian.com>
2018-03-07 11:53:10 +1100

    Rewrite handling deletion for provisioning that is in progress

--- test/integration/controller_instance_test.go
+++ test/integration/controller_instance_test.go
@@ -1184,6 +1184,7 @@ func TestCreateServiceInstanceAsynchronous(t *testing.T) {
 
 func TestDeleteServiceInstance(t *testing.T) {
 	key := osb.OperationKey(testOperation)
+	_ = key
 
 	cases := []struct {
 		name                         string
