afacc112e100c45a9078775c4a48652453ce7fa6
Nail Islamov <nislamov@atlassian.com>
2018-03-07 15:46:55 +1100

    Tests cleanup

--- test/integration/controller_instance_test.go
+++ test/integration/controller_instance_test.go
@@ -1182,7 +1182,6 @@ func TestCreateServiceInstanceAsynchronous(t *testing.T) {
 
 func TestDeleteServiceInstance(t *testing.T) {
 	key := osb.OperationKey(testOperation)
-	_ = key
 
 	cases := []struct {
 		name                         string
