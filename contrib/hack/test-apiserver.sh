3bb13dbbfb57855dcf24a9dbefb5810504c7315d
Erik Nelson <erik@nsk.io>
2018-03-22 08:43:51 -0400

    Add ServiceClass CRUD to integration tests
    NOTE: Failing at this point, something is missing since creates are
    being rejected by the APIServer; it is not expecting to find
    metadata.namespace, so obviously something is missing from the
    ServiceClass implementation.

--- contrib/hack/test-apiserver.sh
+++ contrib/hack/test-apiserver.sh
@@ -43,6 +43,7 @@ NO_TTY=1 kubectl config set-cluster service-catalog-cluster --server=https://${D
 # create a few resources
 set -x
 NO_TTY=1 kubectl create -f contrib/examples/apiserver/broker.yaml
+NO_TTY=1 kubectl create -f contrib/examples/apiserver/clusterserviceclass.yaml
 NO_TTY=1 kubectl create -f contrib/examples/apiserver/serviceclass.yaml
 NO_TTY=1 kubectl create -f contrib/examples/apiserver/serviceplan.yaml
 NO_TTY=1 kubectl create -f contrib/examples/apiserver/instance.yaml
@@ -51,6 +52,7 @@ NO_TTY=1 kubectl create -f contrib/examples/apiserver/podpreset.yaml
 
 NO_TTY=1 kubectl get clusterservicebroker test-broker -o yaml
 NO_TTY=1 kubectl get clusterserviceclass d35b55b2-b1fd-4123-8045-5b9c619cb629 -o yaml
+NO_TTY=1 kubectl get serviceclass d0fe444d-5656-4c7a-ba1b-6c5884eefbb7 -o yaml
 NO_TTY=1 kubectl get clusterserviceplan 10e03cb7-b2cf-40dd-a954-16a382b92446 -o yaml
 NO_TTY=1 kubectl get serviceinstance test-instance --namespace test-ns -o yaml
 NO_TTY=1 kubectl get servicebinding test-binding --namespace test-ns -o yaml
@@ -58,6 +60,7 @@ NO_TTY=1 kubectl get podpresets -o yaml
 NO_TTY=1 kubectl get podpresets db-config -o yaml
 
 NO_TTY=1 kubectl delete -f contrib/examples/apiserver/broker.yaml
+NO_TTY=1 kubectl delete -f contrib/examples/apiserver/clusterserviceclass.yaml
 NO_TTY=1 kubectl delete -f contrib/examples/apiserver/serviceclass.yaml
 NO_TTY=1 kubectl delete -f contrib/examples/apiserver/serviceplan.yaml
 NO_TTY=1 kubectl delete -f contrib/examples/apiserver/instance.yaml
