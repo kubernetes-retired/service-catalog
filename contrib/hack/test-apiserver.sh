2e19b1bc97485bfe8c8c347aa417aaec5066218b
Erik Nelson <erik@nsk.io>
2018-03-22 09:30:13 -0400

    Lookup serviceclass in specific namespace in test
    Make sure the apiserver test is looking for the serviceclass in the
    correct namespace, otherwise it won't find it...because it's namespaced!

--- contrib/hack/test-apiserver.sh
+++ contrib/hack/test-apiserver.sh
@@ -52,7 +52,7 @@ NO_TTY=1 kubectl create -f contrib/examples/apiserver/podpreset.yaml
 
 NO_TTY=1 kubectl get clusterservicebroker test-broker -o yaml
 NO_TTY=1 kubectl get clusterserviceclass d35b55b2-b1fd-4123-8045-5b9c619cb629 -o yaml
-NO_TTY=1 kubectl get serviceclass d0fe444d-5656-4c7a-ba1b-6c5884eefbb7 -o yaml
+NO_TTY=1 kubectl get serviceclass d0fe444d-5656-4c7a-ba1b-6c5884eefbb7 --namespace test-ns -o yaml
 NO_TTY=1 kubectl get clusterserviceplan 10e03cb7-b2cf-40dd-a954-16a382b92446 -o yaml
 NO_TTY=1 kubectl get serviceinstance test-instance --namespace test-ns -o yaml
 NO_TTY=1 kubectl get servicebinding test-binding --namespace test-ns -o yaml
