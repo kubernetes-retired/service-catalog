0a537a4f093fcb43ae609cbf4176080756767490
Jay Boyd <jaboyd@redhat.com>
2018-04-16 13:55:05 -0400

    allow nill service plan during instance last op poll

--- pkg/controller/controller_instance.go
+++ pkg/controller/controller_instance.go
@@ -1545,12 +1545,19 @@ func (c *controller) prepareServiceInstanceLastOperationRequest(instance *v1beta
 		return nil, err
 	}
 
+	// allow for nil ServicePlan which happens if user sets to non-existing plan
+	planID := ""
+	if servicePlan != nil {
+		planID = servicePlan.Spec.ExternalID
+	}
+
 	request := &osb.LastOperationRequest{
 		InstanceID:          instance.Spec.ExternalID,
 		ServiceID:           &serviceClass.Spec.ExternalID,
-		PlanID:              &servicePlan.Spec.ExternalID,
+		PlanID:              &planID,
 		OriginatingIdentity: rh.originatingIdentity,
 	}
+
 	if instance.Status.LastOperation != nil && *instance.Status.LastOperation != "" {
 		key := osb.OperationKey(*instance.Status.LastOperation)
 		request.OperationKey = &key
