d47e88e79f499d05e9604436ea9c4897fe87cb93
teague <tnc1443@gmail.com>
2018-05-17 17:53:24 -0400

    added comments in both types.go

--- pkg/apis/servicecatalog/types.go
+++ pkg/apis/servicecatalog/types.go
@@ -145,10 +145,15 @@ type CommonServiceBrokerSpec struct {
 // ServiceClass allowed property names:
 //   name - the value set to [Cluster]ServiceClass.Name
 //   externalName - the value set to [Cluster]ServiceClass.Spec.ExternalName
+//   externalID - the value set to [Cluster]ServiceClass.Spec.externalID
+//   serviceBrokerName - the value set to [Cluster]ServiceClass.Spec.serviceBrokerName
 //
 // ServicePlan allowed property names:
 //   name - the value set to [Cluster]ServiceClass.Name
 //   externalName - the value set to [Cluster]ServiceClass.Spec.ExternalName
+//   externalID - the value set to [Cluster]ServiceClass.Spec.externalID
+//   serviceClassName - the value set to [Cluster]ServiceClass.Spec.serviceClass.name
+//
 type CatalogRestrictions struct {
 	// ServiceClass represents a selector for plans, used to filter catalog re-lists.
 	ServicePlan []string
--- pkg/apis/servicecatalog/v1beta1/types.go
+++ pkg/apis/servicecatalog/v1beta1/types.go
@@ -166,10 +166,15 @@ type CommonServiceBrokerSpec struct {
 // ServiceClass allowed property names:
 //   name - the value set to [Cluster]ServiceClass.Name
 //   externalName - the value set to [Cluster]ServiceClass.Spec.externalName
+//   externalID - the value set to [Cluster]ServiceClass.Spec.externalID
+//   serviceBrokerName - the value set to [Cluster]ServiceClass.Spec.serviceBrokerName
 //
 // ServicePlan allowed property names:
 //   name - the value set to [Cluster]ServiceClass.Name
 //   externalName - the value set to [Cluster]ServiceClass.Spec.externalName
+//   externalID - the value set to [Cluster]ServiceClass.Spec.externalID
+//   serviceClassName - the value set to [Cluster]ServiceClass.Spec.serviceClass.name
+//
 type CatalogRestrictions struct {
 	// ServiceClass represents a selector for plans, used to filter catalog re-lists.
 	ServiceClass []string `json:"serviceClass,omitempty"`
