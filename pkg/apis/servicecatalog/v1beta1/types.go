a361d83c25c0fe6ec774139ce4a8e870a81772bd
Teague Cole <tnc1443@gmail.com>
2018-05-23 16:16:44 -0400

    Tests for filter.go and comments in types.go (#2055)
    * fetched upstream
    
    * added tests and comments
    
    * spelling
    
    * updated types.go name
    
    * fixed filter cluster servce class and service class name
    
    * fixed tests
    
    * scotts comments 5/21
    
    * go fmt v1beta/types.go

--- pkg/apis/servicecatalog/v1beta1/types.go
+++ pkg/apis/servicecatalog/v1beta1/types.go
@@ -165,11 +165,14 @@ type CommonServiceBrokerSpec struct {
 //
 // ServiceClass allowed property names:
 //   name - the value set to [Cluster]ServiceClass.Name
-//   externalName - the value set to [Cluster]ServiceClass.Spec.ExternalName
-//
+//   spec.externalName - the value set to [Cluster]ServiceClass.Spec.ExternalName
+//   spec.externalID - the value set to [Cluster]ServiceClass.Spec.ExternalID
 // ServicePlan allowed property names:
-//   name - the value set to [Cluster]ServiceClass.Name
-//   externalName - the value set to [Cluster]ServiceClass.Spec.ExternalName
+//   name - the value set to [Cluster]ServicePlan.Name
+//   spec.externalName - the value set to [Cluster]ServicePlan.Spec.ExternalName
+//   spec.externalID - the value set to [Cluster]ServicePlan.Spec.ExternalID
+//   spec.serviceClass.name - the value set to ServicePlan.Spec.ServiceClassRef.Name
+//   spec.clusterServiceClass.name - the vlaue set to ClusterServicePlan.Spec.ClusterServiceClassRef.Name
 type CatalogRestrictions struct {
 	// ServiceClass represents a selector for plans, used to filter catalog re-lists.
 	ServiceClass []string `json:"serviceClass,omitempty"`
