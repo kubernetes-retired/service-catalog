695192e35505613716e69750cd068d873ee93339
Jeff Peeler <jpeeler@redhat.com>
2018-06-07 17:46:47 -0400

    add missing CRD files

---
+++ pkg/apis/settings/doc.go
@@ -0,0 +1,9 @@
+
+
+
+
+// +k8s:deepcopy-gen=package,register
+// +groupName=settings.servicecatalog.k8s.io
+
+// Package api is the internal version of the API.
+package settings
---
+++ pkg/apis/settings/v1alpha1/doc.go
@@ -0,0 +1,10 @@
+// Api versions allow the api contract for a resource to be changed while keeping
+// backward compatibility by support multiple concurrent versions
+// of the same resource
+
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/kubernetes-incubator/service-catalog/pkg/apis/settings
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=settings.servicecatalog.k8s.io
+package v1alpha1 // import "github.com/kubernetes-incubator/service-catalog/pkg/apis/settings/v1alpha1"
