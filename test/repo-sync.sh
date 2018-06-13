6ee08a971be6a23d99ed91c612e5de6de83f1e82
Vic Iglesias <viglesias@google.com>
2018-06-13 20:56:09 +0000

    Make gcloud installation more robust

--- test/repo-sync.sh
+++ test/repo-sync.sh
@@ -24,9 +24,20 @@ tar xzfv ${HELM_TARBALL}
 PATH=`pwd`/linux-amd64/:$PATH
 helm init --client-only
 
+# Install and configure gcloud
+sudo apt-get install -y python
+export PATH=$PATH:/google-cloud-sdk/bin:$PATH
+export CLOUD_SDK_VERSION=204.0.0
+curl -LO "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$CLOUD_SDK_VERSION-linux-x86_64.tar.gz"
+tar xzf "google-cloud-sdk-$CLOUD_SDK_VERSION-linux-x86_64.tar.gz"
+rm "google-cloud-sdk-$CLOUD_SDK_VERSION-linux-x86_64.tar.gz"
+ln -s /lib /lib64
+rm -rf /google-cloud-sdk/.install/.backup
+gcloud version
+gcloud config set core/disable_usage_reporting true
+gcloud config set component_manager/disable_update_check true
+
 # Authenticate before uploading to Google Cloud Storage
-curl https://sdk.cloud.google.com > install-gcloud.sh
-bash install-gcloud.sh --disable-prompts
 gcloud auth activate-service-account --key-file contrib/travis/service-account.json
 
 # Create the repository
