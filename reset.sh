1d21e253de0290707200c546f44b49c129059f8e
Jeff Peeler <jpeeler@redhat.com>
2018-06-19 15:17:33 -0400

    add sleeps to make deletion more reliable

--- reset.sh
+++ reset.sh
@@ -10,11 +10,14 @@ kubectl delete -f pkg/controller/podpreset/webhook/apod-deployment-preset.yaml
 
 kubectl delete -f install.yaml
 
-kubectl delete secret podpreset-service-tls
-
 # mini-walkthrough
 kubectl delete -n test-ns servicebindings ups-binding
+sleep 10
 kubectl delete -n test-ns serviceinstances ups-instance
+sleep 10
 kubectl delete clusterservicebrokers ups-broker
+sleep 10
 helm delete --purge ups-broker
 kubectl delete ns test-ns ups-broker
+
+kubectl delete secret podpreset-service-tls
