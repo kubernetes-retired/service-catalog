a7d29dce77f0e04c15cca7a960aeff3a6ae234d4
Morgan Bauer <mbauer@us.ibm.com>
2018-07-17 13:47:43 -0700

    lint ignores files marked as generated (#2186)
    - add comments to lint hand-made expansions
     - remove client from linter ignores

--- pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1/serviceinstance_expansion.go
+++ pkg/client/clientset_generated/clientset/typed/servicecatalog/v1beta1/serviceinstance_expansion.go
@@ -20,7 +20,7 @@ import (
 	"github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
 )
 
-// The ReferencesExtension interface allows setting the References
+// The ServiceInstanceExpansion interface allows setting the References
 // to ServiceClasses and ServicePlans.
 type ServiceInstanceExpansion interface {
 	UpdateReferences(serviceInstance *v1beta1.ServiceInstance) (*v1beta1.ServiceInstance, error)
