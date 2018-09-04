867a85bb322e357cd0835d942affaf8adfe69788
vivian <yangweiwei@cmss.chinamobile.com>
2018-09-04 23:24:50 +0800

    Fix variable name in reconcileClusterServiceClassKey() (#2312)
    When the clusterserviceclass controller reconciles the key, the
    value should be like 'class' not 'plan'.

--- pkg/controller/controller_clusterserviceclass.go
+++ pkg/controller/controller_clusterserviceclass.go
@@ -55,7 +55,7 @@ func (c *controller) clusterServiceClassDelete(obj interface{}) {
 // reconciliation loop for ClusterServiceClass. ClusterServiceClasses are primarily
 // reconciled in a separate flow when a ClusterServiceBroker is reconciled.
 func (c *controller) reconcileClusterServiceClassKey(key string) error {
-	plan, err := c.clusterServiceClassLister.Get(key)
+	class, err := c.clusterServiceClassLister.Get(key)
 	if errors.IsNotFound(err) {
 		glog.Infof("ClusterServiceClass %q: Not doing work because it has been deleted", key)
 		return nil
@@ -65,7 +65,7 @@ func (c *controller) reconcileClusterServiceClassKey(key string) error {
 		return err
 	}
 
-	return c.reconcileClusterServiceClass(plan)
+	return c.reconcileClusterServiceClass(class)
 }
 
 func (c *controller) reconcileClusterServiceClass(serviceClass *v1beta1.ClusterServiceClass) error {
