274ff86992d542ae7603dae95af95c04385b0727
Rui Cao <ruicao@alauda.io>
2018-09-20 22:13:49 +0800

    Fix some typos (#2351)
    Signed-off-by: Rui Cao <ruicao@alauda.io>

--- cmd/healthcheck/framework/options.go
+++ cmd/healthcheck/framework/options.go
@@ -59,7 +59,7 @@ func NewHealthCheckServer() *HealthCheckServer {
 func (s *HealthCheckServer) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&s.KubeHost, "kubernetes-host", "http://127.0.0.1:8080", "The kubernetes host, or apiserver, to connect to")
 	fs.StringVar(&s.KubeConfig, "kubernetes-config", os.Getenv(clientcmd.RecommendedConfigPathEnvVar), "Path to config containing embedded authinfo for kubernetes. Default value is from environment variable "+clientcmd.RecommendedConfigPathEnvVar)
-	fs.StringVar(&s.KubeContext, "kubernetes-context", "", "config context to use for kuberentes. If unset, will use value from 'current-context'")
+	fs.StringVar(&s.KubeContext, "kubernetes-context", "", "config context to use for kubernetes. If unset, will use value from 'current-context'")
 	fs.DurationVar(&s.HealthCheckInterval, "healthcheck-interval", s.HealthCheckInterval, "How frequently the end to end health check should be performed")
 	fs.StringVar(&s.TestBrokerName, "broker-name", "ups-broker", "Broker Name to test against - can only be ups-broker or osb-stub.  You must ensure the specified broker is deployed.")
 	s.SecureServingOptions.AddFlags(fs)
