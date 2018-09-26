fa58c4f463198caaf04a0a01f8937dc06991f05f
Shubheksha Jalan <jshubheksha@gmail.com>
2018-09-26 22:54:53 +0530

    add unit tests using fake k8s client

--- cmd/svcat/class/describe_cmd_test.go
+++ cmd/svcat/class/describe_cmd_test.go
@@ -24,14 +24,13 @@ import (
 	"github.com/kubernetes-incubator/service-catalog/pkg/svcat"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	k8sfake "k8s.io/client-go/kubernetes/fake"
 )
 
 func TestDescribeCommand(t *testing.T) {
 	const namespace = "default"
 	testcases := []struct {
-		name            string // Test Name
+		name            string // Test Nmae
 		cmd             string // Command to run
 		golden          string // Relative path to a golden file, compared to the command output
 		continueOnError bool   // Should the test stop immediately if the command fails or continue and capture the console output
@@ -39,21 +38,88 @@ func TestDescribeCommand(t *testing.T) {
 		{
 			name:   "describe class by name",
 			cmd:    "describe class user-provided-service",
-			golden: "../output/describe-class.txt",
+			golden: "describe-class.txt",
 		},
 	}
 	for _, tc := range testcases {
 		t.Run(tc.name, func(t *testing.T) {
 			// Setup fake data for the app
 			k8sClient := k8sfake.NewSimpleClientset()
-			var fake runtime.Object
-			fake = &v1beta1.ClusterServiceClass{
+			fakeClass := &v1beta1.ClusterServiceClass{
 				ObjectMeta: v1.ObjectMeta{
-					Name: "user provided service",
+					Name:            "4f6e6cf6-ffdd-425f-a2c7-3c9258ad2468",
+					UID:             "7b3c2fe0-f711-11e7-aa44-0242ac110005",
+					ResourceVersion: "3",
+				},
+				Spec: v1beta1.ClusterServiceClassSpec{
+					ClusterServiceBrokerName: "ups-broker",
+					CommonServiceClassSpec: v1beta1.CommonServiceClassSpec{
+						ExternalName:       "user-provided-service",
+						ExternalID:         "4f6e6cf6-ffdd-425f-a2c7-3c9258ad2468",
+						Description:        "A user provided service",
+						Bindable:           true,
+						BindingRetrievable: false,
+						PlanUpdatable:      true,
+					},
+				},
+				Status: v1beta1.ClusterServiceClassStatus{
+					CommonServiceClassStatus: v1beta1.CommonServiceClassStatus{
+						RemovedFromBrokerCatalog: false,
+					},
 				},
 			}
 
-			svcatClient := svcatfake.NewSimpleClientset(fake)
+			fakePlan1 := &v1beta1.ClusterServicePlan{
+				ObjectMeta: v1.ObjectMeta{
+					Name:            "86064792-7ea2-467b-af93-ac9694d96d52",
+					UID:             "69ce3c3d-f7de-11e7-9c07-0242ac110006",
+					ResourceVersion: "5",
+				},
+				Spec: v1beta1.ClusterServicePlanSpec{
+					ClusterServiceBrokerName: "ups-broker",
+					CommonServicePlanSpec: v1beta1.CommonServicePlanSpec{
+						ExternalName: "default",
+						ExternalID:   "86064792-7ea2-467b-af93-ac9694d96d52",
+						Description:  "Sample plan description",
+						Free:         true,
+					},
+					ClusterServiceClassRef: v1beta1.ClusterObjectReference{
+						Name: "4f6e6cf6-ffdd-425f-a2c7-3c9258ad2468",
+					},
+				},
+				Status: v1beta1.ClusterServicePlanStatus{
+					CommonServicePlanStatus: v1beta1.CommonServicePlanStatus{
+						RemovedFromBrokerCatalog: false,
+					},
+				},
+			}
+
+			fakePlan2 := &v1beta1.ClusterServicePlan{
+				ObjectMeta: v1.ObjectMeta{
+					Name:            "cc0d7529-18e8-416d-8946-6f7456acd589",
+					UID:             "7b497b48-f711-11e7-aa44-0242ac110005",
+					ResourceVersion: "5",
+				},
+				Spec: v1beta1.ClusterServicePlanSpec{
+					ClusterServiceBrokerName: "ups-broker",
+					CommonServicePlanSpec: v1beta1.CommonServicePlanSpec{
+						ExternalName: "premium",
+						ExternalID:   "cc0d7529-18e8-416d-8946-6f7456acd589",
+						Description:  "Premium plan",
+						Free:         true,
+					},
+					ClusterServiceClassRef: v1beta1.ClusterObjectReference{
+						Name: "4f6e6cf6-ffdd-425f-a2c7-3c9258ad2468",
+					},
+				},
+				Status: v1beta1.ClusterServicePlanStatus{
+					CommonServicePlanStatus: v1beta1.CommonServicePlanStatus{
+						RemovedFromBrokerCatalog: false,
+					},
+				},
+			}
+
+			svcatClient := svcatfake.NewSimpleClientset(fakeClass, fakePlan1, fakePlan2)
 			fakeApp, _ := svcat.NewApp(k8sClient, svcatClient, namespace)
 			output := &bytes.Buffer{}
 			cxt := svcattest.NewContext(output, fakeApp)
