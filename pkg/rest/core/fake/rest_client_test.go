cd097a14705c39aa556c90884658a3cda6b22c6d
shouhong <shouhong.zhang@outlook.com>
2018-11-15 23:21:38 +0800

    fix TestResponseWriterGetResponse problem

--- pkg/rest/core/fake/rest_client_test.go
+++ pkg/rest/core/fake/rest_client_test.go
@@ -120,7 +120,7 @@ func TestResponseWriterGetResponse(t *testing.T) {
 	rw.WriteHeader(http.StatusFound)
 
 	if response := rw.getResponse(); response.StatusCode != http.StatusFound {
-		t.Fatal("Expected", 0, "got", response.StatusCode)
+		t.Fatal("Expected", http.StatusFound, "got", response.StatusCode)
 	}
 }
 
