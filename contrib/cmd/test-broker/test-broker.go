cc52993afd6b126e8bad8a3aee82e3db2462f2e6
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-05-17 16:57:14 +0200

    Fix the test-broker flags parsing

--- contrib/cmd/test-broker/test-broker.go
+++ contrib/cmd/test-broker/test-broker.go
@@ -41,10 +41,10 @@ var options struct {
 }
 
 func init() {
-	flags := flag.NewFlagSet("test-broker", flag.ExitOnError)
-	flag.IntVar(&options.Port, "port", 8005, "use '--port' option to specify the port for broker to listen on")
-	flag.StringVar(&options.TLSCert, "tlsCert", "", "base-64 encoded PEM block to use as the certificate for TLS. If '--tlsCert' is used, then '--tlsKey' must also be used. If '--tlsCert' is not used, then TLS will not be used.")
-	flag.StringVar(&options.TLSKey, "tlsKey", "", "base-64 encoded PEM block to use as the private key matching the TLS certificate. If '--tlsKey' is used, then '--tlsCert' must also be used")
+	flags = flag.NewFlagSet("test-broker", flag.ExitOnError)
+	flags.IntVar(&options.Port, "port", 8005, "use '--port' option to specify the port for broker to listen on")
+	flags.StringVar(&options.TLSCert, "tlsCert", "", "base-64 encoded PEM block to use as the certificate for TLS. If '--tlsCert' is used, then '--tlsKey' must also be used. If '--tlsCert' is not used, then TLS will not be used.")
+	flags.StringVar(&options.TLSKey, "tlsKey", "", "base-64 encoded PEM block to use as the private key matching the TLS certificate. If '--tlsKey' is used, then '--tlsCert' must also be used")
 	klog.InitFlags(flags)
 }
 
@@ -53,7 +53,6 @@ func main() {
 	if err != nil {
 		klog.Fatalln(err)
 	}
-	flag.Parse()
 	if err := run(); err != nil && err != context.Canceled && err != context.DeadlineExceeded {
 		klog.Fatalln(err)
 	}
