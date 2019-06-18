b7671afcbfd80d56f0d695deaba5488f006e3221
Jonathan Berkhahn <jaberkha@us.ibm.com>
2019-06-18 07:42:31 -0700

    change controller finalizer back to kubernetes-incubator (#2664)

--- pkg/apis/servicecatalog/v1beta1/types.go
+++ pkg/apis/servicecatalog/v1beta1/types.go
@@ -1319,7 +1319,7 @@ const (
 
 // These are external finalizer values to service catalog, must be qualified name.
 const (
-	FinalizerServiceCatalog string = "kubernetes-sigs/service-catalog"
+	FinalizerServiceCatalog string = "kubernetes-incubator/service-catalog"
 )
 
 // ServiceBindingPropertiesState is the state of a
