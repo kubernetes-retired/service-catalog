303abfaa5386a505f35aebd3e99792ea0006b656
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-08-22 09:48:33 +0200

    Add option to execute e2e test both locally and CI (#2690)
    - Refactor code for running the e2e test. Tests can be executed both on CI and locally by executing `make test-e2e`
    - Remove the outdated Jenkins folder/scripts
    - Update documentation about e2e tests
    - Move all CI scripts to a single folder

--- contrib/hack/ci/publish-helm-chart.sh
+++ contrib/hack/ci/publish-helm-chart.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
 # Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,18 +13,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-HELM_URL=https://storage.googleapis.com/kubernetes-helm
-HELM_TARBALL=helm-v2.9.1-linux-amd64.tar.gz
-SVC_CATALOG_BUCKET=svc-catalog-charts
-SVC_CATALOG_REPO_URL=https://$SVC_CATALOG_BUCKET.storage.googleapis.com/
 
-# Setup Helm
-wget -q ${HELM_URL}/${HELM_TARBALL}
-tar xzfv ${HELM_TARBALL}
-PATH=`pwd`/linux-amd64/:$PATH
+# standard bash error handling
+set -o nounset # treat unset variables as an error and exit immediately.
+set -o errexit # exit immediately when a command fails.
+
+readonly CURRENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+readonly TMP_DIR=$(mktemp -d)
+
+source "${CURRENT_DIR}/lib/utilities.sh" || { echo 'Cannot load CI utilities.'; exit 1; }
+source "${CURRENT_DIR}/lib/deps_ver.sh" || { echo 'Cannot load dependencies versions.'; exit 1; }
+
+readonly SVC_CATALOG_BUCKET=${SVC_CATALOG_BUCKET:-svc-catalog-charts}
+readonly SVC_CATALOG_REPO_URL=https://${SVC_CATALOG_BUCKET}.storage.googleapis.com/
+
+shout "- Setup Helm"
+export INSTALL_DIR=${TMP_DIR} HELM_VERSION=${STABLE_HELM_VERSION}
+install::local::helm
 helm init --client-only
 
-# Install and configure gcloud
+shout "- Install and configure gcloud"
 sudo apt-get install -y python
 export CLOUD_SDK_VERSION=204.0.0
 curl -LO "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$CLOUD_SDK_VERSION-linux-x86_64.tar.gz"
@@ -36,19 +44,22 @@ gcloud version
 gcloud config set core/disable_usage_reporting true
 gcloud config set component_manager/disable_update_check true
 
-# Authenticate before uploading to Google Cloud Storage
-gcloud auth activate-service-account --key-file contrib/travis/gcloud-key-file.json
+shout "- Authenticate to Google Cloud Storage"
+gcloud auth activate-service-account --key-file contrib/hack/ci/assets/gcloud-key-file.json
 
-# Create the repository
+shout "- Create the repository"
 SVC_CATALOG_REPO_DIR=svc-catalog-repo
 mkdir -p ${SVC_CATALOG_REPO_DIR}
-cd ${SVC_CATALOG_REPO_DIR}
-  gsutil cp gs://$SVC_CATALOG_BUCKET/index.yaml .
+pushd ${SVC_CATALOG_REPO_DIR}
+
+  gsutil cp gs://${SVC_CATALOG_BUCKET}/index.yaml .
   for dir in `ls ../charts`;do
-    helm dep build ../charts/$dir
-    helm package ../charts/$dir
+    helm dep build ../charts/${dir}
+    helm package ../charts/${dir}
   done
   helm repo index --url ${SVC_CATALOG_REPO_URL} --merge ./index.yaml .
-  gsutil -m rsync ./ gs://$SVC_CATALOG_BUCKET/
-cd ..
+  gsutil -m rsync ./ gs://${SVC_CATALOG_BUCKET}/
+
+popd
+
 ls -l ${SVC_CATALOG_REPO_DIR}
