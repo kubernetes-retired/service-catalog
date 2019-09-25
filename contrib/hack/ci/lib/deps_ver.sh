30d734021763f45f839a4a7051da006acd0d9582
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-09-25 10:33:48 +0200

    Update the run-migration-tests.sh script

--- contrib/hack/ci/lib/deps_ver.sh
+++ contrib/hack/ci/lib/deps_ver.sh
@@ -14,5 +14,6 @@
 
 # Upgrade binary versions in a controlled fashion
 # along with the script contents (config, flags...)
-readonly STABLE_KIND_VERSION=v0.4.0
+readonly STABLE_KUBERNETES_VERSION=v1.15.3
+readonly STABLE_KIND_VERSION=v0.5.1
 readonly STABLE_HELM_VERSION=v2.14.3
