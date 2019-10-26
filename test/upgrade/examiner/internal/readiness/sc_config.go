a45ee1bd83470957bd76306fd9cc029912fe905a
sph <shepenghui1@huawei.com>
2019-10-26 15:14:45 +0800

    fix golint errors
    Signed-off-by: sph <shepenghui1@huawei.com>

--- test/upgrade/examiner/internal/readiness/sc_config.go
+++ test/upgrade/examiner/internal/readiness/sc_config.go
@@ -18,7 +18,7 @@ package readiness
 
 // ServiceCatalogConfig collects all parameters from env variables required to run upgrade tests
 type ServiceCatalogConfig struct {
-	ServiceCatalogApiServerName        string `envconfig:"SC_APISERVER"`
+	ServiceCatalogAPIServerName        string `envconfig:"SC_APISERVER"`
 	ServiceCatalogControllerServerName string `envconfig:"SC_CONTROLLER"`
 	ServiceCatalogNamespace            string `envconfig:"SC_NAMESPACE"`
 	TestBrokerName                     string `envconfig:"TB_NAME"`
